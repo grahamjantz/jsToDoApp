5b22661e7a93ac2a2cbae8ca088f366f89be8ec5
grahamjantz <grahammrjantz@gmail.com>
2022-07-06 15:27:56 -0600

    recreate javascript functionality and improve css styling

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
-let addToDoButton = document.getElementById('add-to-do');
-let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
- 
- addToDoButton.addEventListener('click', function(){
- 	var paragraph = document.createElement('p');
- 	paragraph.innerText = inputField.value;
- 	paragraph.classList.add('paragraph-styling');
- 	toDoContainer.appendChild(paragraph);
- 	inputField.value = "";
- 	paragraph.addEventListener('click', function(){
- 		paragraph.style.textDecoration = 'line-through';
- 	})
- 	paragraph.addEventListener('dblclick', function(){
- 		toDoContainer.removeChild(paragraph);
- 	})
- });
+let addButton = document.getElementById('add-to-do');
+let toDoItems = document.getElementById('toDoItems');
+
+addButton.addEventListener('click', function() {
+	let paragraph = inputField.value;
+	let newLi = document.createElement('li');
+	let deleteButton = document.createElement('button');
+	let newPara = document.createElement('p');
+	newPara.innerHTML = paragraph;
+	newPara.setAttribute('id', 'newPara')
+	newLi.setAttribute('id', 'toDoItem')
+	deleteButton.setAttribute('id', 'xButton')
+	deleteButton.innerHTML = 'x';
+	newLi.appendChild(newPara);
+	newLi.appendChild(deleteButton);
+	toDoItems.appendChild(newLi);
+	inputField.value = '';
+	deleteButton.addEventListener('click', function() {
+		toDoItems.removeChild(newLi);
+	})
+})
+
+
+
