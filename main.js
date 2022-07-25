abcef2651fc3b579942cbf320f0692becd78966a
grahamjantz <grahammrjantz@gmail.com>
2022-07-25 17:16:12 -0600

    begin adding further functionality by selecting month you want item completed in

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 let inputField = document.getElementById('inputField');
 let addButton = document.getElementById('add-to-do');
 let toDoItems = document.getElementById('toDoItems');
+let toDoInMonthHeader = [
+	document.getElementById('toDoInMonthHeaderJul'),
+	document.getElementById('toDoInMonthHeaderAug'),
+	document.getElementById('toDoInMonthHeaderSept'),
+	document.getElementById('toDoInMonthHeaderOct'),
+	document.getElementById('toDoInMonthHeaderNov'),
+	document.getElementById('toDoInMonthHeaderDec'),
+	document.getElementById('toDoInMonthHeaderJan'),
+	document.getElementById('toDoInMonthHeaderFeb'),
+	document.getElementById('toDoInMonthHeaderMar'),
+	document.getElementById('toDoInMonthHeaderApr'),
+	document.getElementById('toDoInMonthHeaderMay')];
+let toDoInMonthHeaderStyling = document.getElementsByClassName('to-do-in-month-header');
 
 addButton.addEventListener('click', function() {
 	let paragraph = inputField.value;
@@ -19,7 +32,22 @@ addButton.addEventListener('click', function() {
 	deleteButton.addEventListener('click', function() {
 		toDoItems.removeChild(newLi);
 	})
+	addMonthHeader();
 })
 
-
-
+const addMonthHeader = () => {
+	if(toDoInMonthHeader[0]) {
+		console.log('july');
+		toDoInMonthHeaderStyling.style.display = 'block';
+	}
+	if(toDoInMonthHeader[1]) {}
+	if(toDoInMonthHeader[2]) {}
+	if(toDoInMonthHeader[3]) {}
+	if(toDoInMonthHeader[4]) {}
+	if(toDoInMonthHeader[5]) {}
+	if(toDoInMonthHeader[6]) {}
+	if(toDoInMonthHeader[7]) {}
+	if(toDoInMonthHeader[8]) {}
+	if(toDoInMonthHeader[9]) {}
+	if(toDoInMonthHeader[10]) {}
+}
